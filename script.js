db4733b1f7c661d20e5bd6f3061bcd93a71025ce
Kleyr <chanklearbaylon@gmail.com>
2024-05-22 00:35:32 +0800

    Made adjustments/Removed contact form.

---
+++ script.js
@@ -0,0 +1,44 @@
+    // TYPING TEXT ANIMATION
+
+    const typed = new Typed(".spanname", {
+    strings: ["Chan Klear Baylon", "Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+
+    // NAVBAR FIXED WHEN SCROLL
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const navbar = document.querySelector('.header');
+        const navbarHeight = navbar.offsetHeight;
+
+        function navbarSticky() {
+            if (window.pageYOffset > navbarHeight) {
+                navbar.classList.add('navbarsticked');
+            } else {
+                navbar.classList.remove('navbarsticked');
+                }
+            }
+
+        window.addEventListener('scroll', navbarSticky);
+});
+
+    // REVEAL ON SCROLL
+        
+    document.addEventListener('DOMContentLoaded', () => {
+        const revealElements = document.querySelectorAll('.reveal');
+    
+        function revealOnScroll() {
+            revealElements.forEach(function(element) {
+                const revealPosition = element.getBoundingClientRect().top;
+                const windowHeight = window.innerHeight;
+    
+                if (revealPosition < windowHeight / 1.6) {
+                    element.classList.add('revealed');
+                }
+            });
+        }
+    
+        window.addEventListener('scroll', revealOnScroll);
+});
